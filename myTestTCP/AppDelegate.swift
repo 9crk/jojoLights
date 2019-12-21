a50b364342ac50e19247a3d9515491153da98e90
9crk <admin@9crk.com>
2019-12-21 11:28:49 +0800

    Initial Commit

---
+++ myTestTCP/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  myTestTCP
+//
+//  Created by zhouhua on 2019/12/21.
+//  Copyright © 2019 zhouhua. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ myTestTCP/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  myTestTCP
+//
+//  Created by zhouhua on 2019/12/21.
+//  Copyright © 2019 zhouhua. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
